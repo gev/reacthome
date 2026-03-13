13875dac12a01db68697837e8925153671fa6565
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-14 00:10:27 +0300

    Simplify logic server. Remove `id`

--- logic/src/Reacthome/Logic.hs
+++
@@ -1,8 +0,0 @@
-module Reacthome.Logic where
-
-import Data.ByteString qualified as S
-import Data.ByteString.Lazy qualified as L
-
-type Uid = S.ByteString
-type StrictRaw = S.ByteString
-type LazyRaw = L.ByteString
--- logic/src/Reacthome/Logic/Server.hs
+++ logic/src/Reacthome/Logic/Server.hs
@@ -3,7 +3,6 @@ module Reacthome.Logic.Server where
 import Control.Concurrent.Async (race)
 import Control.Concurrent.Chan.Unagi.Bounded (newChan, tryRead, tryReadChan, writeChan)
 import Data.ByteString qualified as BS
-import Data.UUID (UUID, toByteString)
 import Reacthome.Logic.Error (LogicError (..), logError)
 import WebSockets.Connection (WebSocketConnection (..))
 import WebSockets.Options (WebSocketOptions)
@@ -11,39 +10,34 @@ import WebSockets.PendingConnection (WebSocketPendingConnection (..))
 import Prelude hiding (lookup, take)
 
 newtype LogicServer = LogicServer
-    { accept :: WebSocketPendingConnection -> Peer -> IO ()
+    { accept :: WebSocketPendingConnection -> IO ()
     }
-
-type Peer = UUID
 
 makeLogicServer ::
     (?options :: WebSocketOptions) =>
     IO LogicServer
 makeLogicServer = do
     let
-        accept pending peer = do
-            (inChan, outChan) <- newChan 10
-            let
-                sink message = do
-                    main <- BS.readFile "./logic/glue/main.glue"
-                    writeChan inChan $ "(put store.cache \"main\" " <> main <> ")"
-                    next <- BS.readFile "./logic/glue/next.glue"
-                    writeChan inChan $ "(put store.cache \"next\" " <> next <> ")"
-                source = do
-                    (!element, !wait) <- tryReadChan outChan
-                    !message <- tryRead element
-                    pure (message, wait)
-
-                from = BS.toStrict $ toByteString peer
-
+        accept pending = do
             !successful <- pending.accept
             case successful of
-                Left !e -> logError $ WebSocketError from e
+                Left !e -> logError $ WebSocketError e
                 Right !connection -> do
+                    (inChan, outChan) <- newChan 10
+                    let
+                        sink message = do
+                            main <- BS.readFile "./logic/glue/main.glue"
+                            writeChan inChan $ "(put store.cache \"main\" " <> main <> ")"
+                            next <- BS.readFile "./logic/glue/next.glue"
+                            writeChan inChan $ "(put store.cache \"next\" " <> next <> ")"
+                        source = do
+                            (!element, !wait) <- tryReadChan outChan
+                            !message <- tryRead element
+                            pure (message, wait)
                     res <-
                         either id id <$> race
                             do connection.runReceiveMessageLoop sink
                             do connection.runSendMessageLoop source
-                    logError $ WebSocketError from res
+                    logError $ WebSocketError res
     pure
         LogicServer{..}
