4282d872526b017cf9631a7d6aa03dc3fbd8626d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-04-11 19:53:11 +0300

    Add more fetailed errors

--- relay/src/Reacthome/Relay/Error.hs
+++ relay/src/Reacthome/Relay/Error.hs
@@ -1,15 +1,19 @@
 module Reacthome.Relay.Error where
 
 import Control.Exception (Exception)
+import Data.ByteString (ByteString)
 import Data.Text (Text)
 import Debug.Trace (traceIO)
-import Reacthome.Relay (StrictRaw, Uid)
+import Reacthome.Relay (Uid)
 import WebSockets.Error (WebSocketError)
 import Prelude hiding (error)
 
 data RelayError
-    = InvalidUUID Text
-    | InvalidDestination StrictRaw
+    = InvalidUri ByteString
+    | InvalidVersion Text
+    | NoPeerPresent
+    | InvalidPeer Text
+    | InvalidDestination Uid
     | NoPeersFound Uid
     | WebSocketError Uid WebSocketError
     | InvalidMessageLength
@@ -24,16 +28,22 @@ logError :: RelayError -> IO ()
 logError err =
     traceIO $
         "[ERROR] " <> case err of
+            InvalidUri uri ->
+                "Invalid URI: " <> show uri
+            InvalidVersion version ->
+                "Invalid version: " <> show version
+            NoPeerPresent ->
+                "No peer present"
+            InvalidPeer peer ->
+                "Invalid peer UUID: " <> show peer
+            InvalidDestination uid ->
+                "Invalid destination UUID: " <> show uid
+            NoPeersFound peer ->
+                "No peers found for UUID: " <> show peer
+            WebSocketError peer e ->
+                "WebSocket error, peer " <> show peer <> ": " <> show e
             InvalidMessageLength{..} ->
                 "Invalid message length: got "
                     <> show messageLength
                     <> ", expected at least "
                     <> show minimumLength
-            InvalidUUID bytes ->
-                "Invalid UUID in message: " <> show bytes
-            InvalidDestination uid ->
-                "Invalid UUID in message: " <> show uid
-            NoPeersFound peer ->
-                "No relays found for UUID: " <> show peer
-            WebSocketError peer e ->
-                "WebSocket error, peer " <> show peer <> ": " <> show e
