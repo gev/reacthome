1b57aa3a0f1cc26e3103a71eed675d74110b9307
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-04-26 13:54:05 +0300

    clean up neiro slop in the discovery project

--- packages/discovery/app/Main.hs
+++ packages/discovery/app/Main.hs
@@ -1,31 +1,3 @@
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever, void)
-import Discovery.Anoncer (startAnoncer)
-import Discovery.Responder (startResponder)
-import Discovery.Scanner (startScanner)
-
 main :: IO ()
 main = do
-    putStrLn "Starting Discovery System..."
-
-    -- Start responder (listens for discovery requests and responds with unicast)
-    void $ forkIO $ do
-        putStrLn "Starting Responder..."
-        startResponder "TestService:192.168.1.100:8080"
-
-    threadDelay 500_000 -- Small delay to ensure responder is ready
-
-    -- Start anoncer (periodically announces to the announcement group)
-    void $ forkIO $ do
-        putStrLn "Starting Anoncer..."
-        startAnoncer "TestService:192.168.1.100:8080"
-
-    threadDelay 500_000 -- Small delay to ensure anoncer is running
-
-    -- Start scanner (sends discovery requests and listens for announcements)
-    putStrLn "Starting Scanner..."
-    scanner
-  where
-    scanner = forever $ do
-        startScanner
-        threadDelay 3_000_000 -- Restart scanner every 30 seconds
+    putStrLn "Multicast Discovery System"
--- packages/discovery/src/Discovery/Anoncer.hs
+++ packages/discovery/src/Discovery/Anoncer.hs
@@ -1,35 +1,3 @@
 module Discovery.Anoncer (
-    startAnoncer,
-    announcementHost,
-    announcementPort,
+
 ) where
-
-import Control.Concurrent (threadDelay)
-import Control.Exception qualified as E
-import Control.Monad (forever, void)
-import Data.ByteString.Char8 qualified as C
-import Data.List.NonEmpty qualified as NE
-import Network.Socket
-import Network.Socket.ByteString (sendTo)
-
--- Announcements address (localhost UDP)
-announcementHost :: String
-announcementHost = "192.168.11.210"
-
-announcementPort :: String
-announcementPort = "5001"
-
--- Start the announcer - periodically sends announcements to the announcement port
-startAnoncer :: String -> IO ()
-startAnoncer serviceName = do
-    addr <- resolveAddr
-    E.bracket (openSocket addr) close $ \sock -> do
-        forever $ do
-            let announcement = C.pack $ "ANNOUNCE:" ++ serviceName
-            void $ sendTo sock announcement (addrAddress addr)
-            putStrLn $ "Anoncer: sent announcement for " ++ serviceName
-            threadDelay 5_000_000 -- Send announcement every 5 seconds
-  where
-    resolveAddr = do
-        let hints = defaultHints{addrSocketType = Datagram, addrFamily = AF_INET}
-        NE.head <$> getAddrInfo (Just hints) (Just announcementHost) (Just announcementPort)
