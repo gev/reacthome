227f8dd93ece4c193cb99075de0e26aaddbfa357
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-07 21:56:01 +0300

    move out util functions

--- packages/discovery/src/Discovery/Annoncer.hs
+++ packages/discovery/src/Discovery/Annoncer.hs
@@ -6,31 +6,21 @@ import Control.Concurrent
 import Control.Exception
 import Control.Monad
 import Data.Foldable
-import Data.List.NonEmpty qualified as NE
-import Network.Info
+import Discovery.Utils
 import Network.Socket
 import Network.Socket.ByteString
 
 runAnnoncer :: HostName -> ServiceName -> IO ()
 runAnnoncer group port = do
-    group' <- resolve group
+    group' <- resolve group port
     forever
         do
-            ifaces <- filter real <$> getNetworkInterfaces
+            ifaces <- getInterfaces
             for_ ifaces \iface -> do
-                host' <- resolve iface.name
+                host' <- resolve iface port
                 handle @SomeException print do
                     bracket (openSocket host') close \sock -> do
                         setSockOpt sock MulticastLoop False
-                        putStrLn $ "sending hello to " <> iface.name
+                        putStrLn $ "sending hello to " <> iface
                         sendAllTo sock "hello" group'.addrAddress
             threadDelay 1_000_000
-  where
-    resolve addr =
-        NE.head
-            <$> getAddrInfo
-                (Just defaultHints{addrFamily = AF_INET, addrSocketType = Datagram})
-                (Just addr)
-                (Just port)
-
-    real NetworkInterface{..} = ipv4 /= IPv4 0 && mac /= MAC 0 0 0 0 0 0
--- packages/discovery/src/Discovery/Scanner.hs
+++ packages/discovery/src/Discovery/Scanner.hs
@@ -4,37 +4,24 @@ module Discovery.Scanner (
 
 import Control.Exception
 import Control.Monad
-import Data.List.NonEmpty qualified as NE
 import Network.Socket
 
 import Control.Concurrent (threadDelay)
+import Discovery.Utils
 import Network.Socket.ByteString
 
 runScanner :: HostName -> ServiceName -> IO ()
 runScanner group port = do
-    host' <- resolve "0.0.0.0"
-    group' <- resolve group
+    hostInfo <- resolve "0.0.0.0" port
+    groupAddr <- hostAddress . addrAddress <$> resolve group port
     forever do
         handle @SomeException print $ do
-            let multicast = MulticastGroup (hostAddress group'.addrAddress) Nothing
-            bracket (openSocket host') close \sock -> do
-                setSockOpt sock ReuseAddr True
+            let multicast = MulticastGroup groupAddr Nothing
+            bracket (openSocket hostInfo) close \sock -> do
                 setSockOpt sock ReusePort True
-                bind sock host'.addrAddress
+                bind sock hostInfo.addrAddress
                 setSockOpt sock AddMembership multicast
                 forever do
                     (msg, addr) <- recvFrom sock 1024
                     print $ show addr <> ": " <> show msg
-
         threadDelay 1_000_000
-  where
-    resolve addr =
-        NE.head
-            <$> getAddrInfo
-                (Just defaultHints{addrFamily = AF_INET, addrSocketType = Datagram})
-                (Just addr)
-                (Just port)
-
-hostAddress :: SockAddr -> HostAddress
-hostAddress (SockAddrInet _ addr) = addr
-hostAddress _ = error "Unsupported socket address"
