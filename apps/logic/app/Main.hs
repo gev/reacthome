d41d69f739b28a0d937bfcb6c25a3a2cd1e8dc12
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-23 01:14:45 +0300

    Make reactive `Store`

--- apps/logic/app/Main.hs
+++ apps/logic/app/Main.hs
@@ -1,20 +1,29 @@
 import Reacthome.Logic.Glue.Publisher (makeGluePublisher)
 import Reacthome.Logic.Glue.Sink (makeSinkRegistry)
-import Reacthome.Logic.Glue.Store (makeGlueStore)
+import Reacthome.Logic.Glue.Store (GlueStore (..), makeGlueStore)
+import Reacthome.Logic.PubSub.Publisher (Publisher (..))
 import Reacthome.Logic.Server (logicServer)
 import WebSockets.Options (defaultWebSocketOptions)
 import WebSockets.Server (runWebSocketServer)
 
 main :: IO ()
 main = do
-    let host = "127.0.0.1"
+    let path = "./apps/logic/glue/"
+        host = "127.0.0.1"
         port = 3005
+
     sinks <- makeSinkRegistry
     let ?sinks = sinks
-    let ?store = makeGlueStore "./apps/logic/glue/"
+
+    let ?store = makeGlueStore path
+
     pubsub <- makeGluePublisher
+    let ?pubsub = pubsub
+
+    ?store.runWatcher pubsub.publish
+
     let ?options = defaultWebSocketOptions
+
     putStrLn $ "Run Reacthome Relay on " <> host <> ":" <> show port
-    runWebSocketServer host port do
-        let ?pubsub = pubsub
-        logicServer
+
+    runWebSocketServer host port logicServer
--- apps/logic/src/Reacthome/Logic/Glue/Store.hs
+++ apps/logic/src/Reacthome/Logic/Glue/Store.hs
@@ -1,20 +1,42 @@
 module Reacthome.Logic.Glue.Store where
 
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Exception (SomeException, catch)
+import Control.Monad (forever, void, when)
 import Data.ByteString.Lazy qualified as L
-import Data.Text (Text, unpack)
+import Data.Text (Text, pack, unpack)
+import System.Directory (canonicalizePath)
+import System.FSNotify (Event (..), EventIsDirectory (..), watchDir, withManager)
 
-newtype GlueStore = GlueStore {get :: Text -> IO (Maybe L.ByteString)}
+data GlueStore = GlueStore
+    { get :: Text -> IO (Maybe L.ByteString)
+    , runWatcher :: (Text -> L.ByteString -> IO ()) -> IO ()
+    }
 
 makeGlueStore :: String -> GlueStore
-makeGlueStore folder =
-    GlueStore
-        { get = \name -> catch @SomeException
-            do
-                let file = folder <> unpack name <> ".glue"
-                glue <- L.readFile file
-                pure $ Just glue
-            \err -> do
-                print err
-                pure Nothing
-        }
+makeGlueStore folder = GlueStore{..}
+  where
+    get name = getFile $ folder <> unpack name <> ".glue"
+
+    runWatcher publish = void . forkIO $ withManager \mgr -> do
+        void $ watchDir mgr folder (const True) (handle publish)
+        forever $ threadDelay 1_000_000
+
+    handle publish (Modified path _ IsFile) =
+        getFile path >>= \case
+            Nothing -> print $ "File not found: " <> path
+            Just value -> do
+                absolute <- canonicalizePath folder
+                let file = drop (length absolute + 1) path
+                let (key, ext) = splitAt (length file - 5) file
+                when (ext == ".glue") do
+                    publish (pack key) value
+    handle _ _ = pure ()
+
+    getFile file = catch @SomeException
+        do
+            glue <- L.readFile file
+            pure $ Just glue
+        \err -> do
+            print err
+            pure Nothing
