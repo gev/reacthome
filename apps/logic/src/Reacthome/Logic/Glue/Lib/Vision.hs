236ed56b9d8f8717f2ec28a247fcd3169b88751c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-26 14:13:41 +0300

    Log module

--- apps/logic/src/Reacthome/Logic/Glue/Lib/Vision.hs
+++ apps/logic/src/Reacthome/Logic/Glue/Lib/Vision.hs
@@ -3,8 +3,10 @@ module Reacthome.Logic.Glue.Lib.Vision where
 import Data.UUID (UUID)
 import Glue.Eval (Eval)
 import Glue.Module (ModuleInfo, nativeModule)
-import Reacthome.Logic.Glue.Lib.Vision.Get
+import Reacthome.Logic.Glue.Lib.Vision.Get (get)
+import Reacthome.Logic.Glue.Lib.Vision.Log (log)
 import Reacthome.Logic.Glue.Publisher (GluePublisher)
+import Prelude hiding (log)
 
 vision ::
     ( ?session :: UUID
@@ -15,4 +17,5 @@ vision =
     nativeModule
         "vision"
         [ ("get", get)
+        , ("log", log)
         ]
