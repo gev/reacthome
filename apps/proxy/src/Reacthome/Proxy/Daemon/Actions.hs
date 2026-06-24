756b188577c1568ff4c8edbecb23fa25b1e828d5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-24 03:12:29 +0300

    Refactor makeObject to return AST

--- apps/proxy/src/Reacthome/Proxy/Daemon/Actions.hs
+++ apps/proxy/src/Reacthome/Proxy/Daemon/Actions.hs
@@ -19,11 +19,8 @@ convert o =
         "ACTION_SET" -> do
             id' <- lookupId o
             payload <- lookupPayload o
-            makeObject id' payload
+            pure $ makeObject id' payload
         _ -> Nothing
-
-makeObject :: Text -> A.Object -> Maybe AST
-makeObject id' payload = pure . Object $ [("id", String id')] <> props payload
 
 lookupType :: A.Object -> Maybe Text
 lookupType = lookupString "type"
@@ -31,22 +28,25 @@ lookupType = lookupString "type"
 lookupId :: A.Object -> Maybe Text
 lookupId = lookupString "id"
 
+lookupString :: A.Key -> A.Object -> Maybe Text
+lookupString name o =
+    A.lookup name o >>= \case
+        A.String t -> pure t
+        _ -> Nothing
+
 lookupPayload :: A.Object -> Maybe A.Object
 lookupPayload o =
     A.lookup "payload" o >>= \case
         A.Object p -> pure p
         _ -> Nothing
 
+makeObject :: Text -> A.Object -> AST
+makeObject id' payload = Object $ [("id", String id')] <> props payload
+
 props :: A.Object -> [(Text, AST)]
 props payload = bimap A.toText fromAeson <$> A.toList filtered
   where
     filtered = A.filterWithKey (const . (`elem` allowed)) payload
-
-lookupString :: A.Key -> A.Object -> Maybe Text
-lookupString name o =
-    A.lookup name o >>= \case
-        A.String t -> pure t
-        _ -> Nothing
 
 fromAeson :: A.Value -> AST
 fromAeson = \case
