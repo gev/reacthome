b71f07b673985c4144e7c80d0cabed085d6e1bad
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-24 23:55:22 +0300

    Refactor makeUpstream to include discovery parameter and announce changes for specific daemon

--- apps/proxy/src/Reacthome/Proxy/Bridge/Upstream.hs
+++ apps/proxy/src/Reacthome/Proxy/Bridge/Upstream.hs
@@ -1,13 +1,16 @@
 module Reacthome.Proxy.Bridge.Upstream where
 
+import Control.Monad (when)
 import Data.ByteString.Lazy (ByteString)
-import Data.Text.Lazy.Encoding
-import Glue.AST (AST (Object))
+import Data.Text.Lazy.Encoding (encodeUtf8)
+import Glue.AST (AST (..))
 import Glue.Serialize (serializeAST)
 import PubSub.Publisher (Publisher (..))
 import PubSub.Revision (Revision (..))
 import Reacthome.Proxy.Bridge.Cache (Cache (..))
+import Reacthome.Proxy.Config (ProxyConfig (..))
 import Reacthome.Proxy.Daemon.Actions.Decode (decodeAction)
+import Reacthome.Proxy.Discovery (Discovery (..))
 import Reacthome.Proxy.Glue.PubSub.Types (GluePublisher)
 
 newtype Upstream = Upstream
@@ -17,14 +20,18 @@ newtype Upstream = Upstream
 makeUpstream ::
     ( ?cache :: Cache
     , ?pubsub :: GluePublisher
+    , ?discovery :: Discovery
     ) =>
-    Upstream
-makeUpstream =
+    ProxyConfig -> Upstream
+makeUpstream config =
     let
         publish action =
             case decodeAction action of
                 Just (uid, delta, version) -> do
                     value <- ?cache.patch uid delta
+                    when
+                        (uid == config.daemon)
+                        do ?discovery.justAnnounce (extractPayload value)
                     let key = ["proxy", uid]
                         payload = encodeUtf8 $ serializeAST $ Object value
                         revision = Revision{..}
@@ -32,3 +39,8 @@ makeUpstream =
                 Nothing -> pure ()
      in
         Upstream{..}
+  where
+    extractPayload =
+        filter \(name, _) ->
+            name == "title"
+                || name == "code"
